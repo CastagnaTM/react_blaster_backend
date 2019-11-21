111e50df77fe4301346feafca4aa0ad2354dda0a
castagnaTM <castagna.tm@gmail.com>
2019-11-20 19:02:22 -0500

    fixed CORS

--- app/controllers/levels_controller.rb
+++ app/controllers/levels_controller.rb
@@ -1,7 +1,14 @@
 class LevelsController < ApplicationController
 
     def index
-        @levels = Levels.all
+        @levels = Level.all
+        render json:@levels
     end
-    
+
+    def select
+        byebug
+        level = level.find_by(name: params[:name])
+        render json: {level: level}
+    end
+
 end
